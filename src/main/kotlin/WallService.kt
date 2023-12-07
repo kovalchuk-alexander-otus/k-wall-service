fc9e18d345c113d8ffc51162f7ee8d5ba17aaae9
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-12-08 01:59:22 +0300

    second teil

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,8 +1,46 @@
 class WallService {
+    private var id: Int = 0
     private var posts: Array<Post> = emptyArray<Post>()
 
-    fun add(post: Post): Post{
+    /**
+     * Создание записи
+     *
+     * добавлять запись в массив, но при этом назначать посту уникальный среди всех постов идентификатор;
+     * возвращать пост с уже выставленным идентификатором.
+     */
+    fun add(post: Post): Post {
+        post.id = ++id;
         posts += post
         return posts.last()
+    }
+
+    /**
+     * Обновление записи
+     */
+    fun update(post: Post): Boolean {
+        posts.forEachIndexed { i, p ->
+            if (p.id == post.id) {
+                posts[i] = p.copy(
+                    toId = post.toId,
+                    fromId = post.fromId,
+                    text = post.text,
+                    comments = post.comments,
+                    likes = post.likes,
+                    views = post.views,
+                    canPin = post.canPin,
+                    canDelete = post.canDelete,
+                    canEdit = post.canEdit
+                )
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * Пролистать все сообщения
+     */
+    fun showAll() {
+        posts.forEach(::println)
     }
 }
