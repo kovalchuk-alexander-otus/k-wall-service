e38f7e0b752911346c47ff7a24b3330d774d6a2a
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-12-10 16:35:33 +0300

    пометил часть свойств nullable .. значения по-умолчанию оставил, правда заменил на null, потому что без этого, тесты надо переписывать, видимо явно задавая значения

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -5,11 +5,11 @@ data class Post(
     var toId: UInt, // Идентификатор владельца стены, на которой размещена запись. В версиях API ниже 5.7 это поле называется to_id
     val fromId: UInt, // Идентификатор автора записи (от чьего имени опубликована запись)
     var text: String, // Текст записи
-    var comments: Array<Comment> = emptyArray<Comment>(), // Информация о комментариях к записи
-    var likes: Array<Like> = emptyArray<Like>(), // Информация о лайках к записи
+    var comments: Array<Comment>? = null, // Информация о комментариях к записи
+    var likes: Array<Like>? = emptyArray<Like>(), // Информация о лайках к записи
     var views: Array<View> = emptyArray<View>(), // Информация о просмотрах записи
     val canPin: Boolean = false, // Информация о том, может ли текущий пользователь закрепить запись
     val canDelete: Boolean = false, // Информация о том, может ли текущий пользователь удалить запись
     val canEdit: Boolean = false, // Информация о том, может ли текущий пользователь редактировать запись
-    var id: Int = 0 // Идентификатор записи
+    var id: Int? = null // Идентификатор записи
 )
